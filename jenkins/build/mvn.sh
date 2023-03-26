8ee249a6797d60a54cb6ec81714f5065cb20122b
Irfan Khan <irfansjbcse@gmail.com>
2023-03-26 17:15:15 +0530

    updated path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,7 +5,8 @@ echo "** Building jar ***********"
 echo "***************************"
 #docker run  -v /home/vagrant/jenkins_data/pipeline/java-app:/app maven:3-alpine sh
 
-WORKSPACE=/home/vagrant/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/vagrant/jenkins_data/jenkins_home/workspace/maven-docker-pipeline
+          
 
 #docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
 
-WORKSPACE=/home/vagrant/jenkins-data/jenkins_home/workspace/maven-docker-pipeline
+WORKSPACE=/home/vagrant/jenkins_data/jenkins_home/workspace/maven-docker-pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
